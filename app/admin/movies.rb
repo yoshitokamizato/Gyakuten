187731024fa0b8786c9e5053fd3d5771b5e76610
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-11-07 09:11:05 +0900

    add movies

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Program do
+ActiveAdmin.register Movie do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_021454) do
+ActiveRecord::Schema.define(version: 2018_11_07_000456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,14 @@ ActiveRecord::Schema.define(version: 2018_10_17_021454) do
   create_table "errors", force: :cascade do |t|
     t.string "name"
     t.integer "program_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.text "title"
+    t.text "contents"
+    t.text "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
