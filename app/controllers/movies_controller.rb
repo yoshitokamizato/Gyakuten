d15f1bbb1238abb59748e5753ec3d72634cc0627
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-11-07 10:34:10 +0900

    add seed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,16 @@
 class MoviesController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @movies = Movie.all
   end
+
   def show
     @movies = Movie.all
   end
+
+  def move_to_index
+    redirect_to action: :index, flash: {error: "ログインしてください"} unless user_signed_in?
+  end
+
 end
