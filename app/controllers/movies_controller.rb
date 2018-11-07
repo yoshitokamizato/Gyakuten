b713fa6a4c055c62537c72db9971225a0c57aa69
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-11-07 09:26:48 +0900

    add show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
   def index
-    @movie = <<~TEXT
-     <iframe width="560" height="315" src="https://www.youtube.com/embed/FIfbOSy1Bzg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-     TEXT
+    @movies = Movie.all
+  end
+  def show
+    @movies = Movie.all
   end
 end
