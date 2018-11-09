dfb030a0bc6ec8c1192167689785499314968b14
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-11-09 17:14:06 +0900

    edit style

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,5 +1,4 @@
 class LinesController < ApplicationController
-  before_action :move_to_index, except: :index
 
   def index
     @lines = Line.all
@@ -7,10 +6,6 @@ class LinesController < ApplicationController
 
   def show
     @line = Line.find(params[:id])
-  end
-
-  def move_to_index
-    redirect_to action: :index, flash: {error: "ログインしてください"} unless user_signed_in?
   end
 
 end
