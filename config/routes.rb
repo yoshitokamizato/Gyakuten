6cd1cce49b0ca2187f69137654f63242da9658df
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-11-09 09:09:04 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "users_webs#index"
+  root to: "contents#index"
 
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
