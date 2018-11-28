e04f116203a14f6b836765a2262169492b8363a3
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-11-28 20:01:39 +0900

    modify import method

--- app/models/Import.rb
+++ app/models/Import.rb
@@ -13,13 +13,29 @@ class Import
           genre: row["Genre"]
       }
     end
-    begin
-      puts "start import"
-      Movie.create!(data_list)
-      puts "success import"
-    rescue ActiveModel::UnknownAttributeError => invalid
-      puts "failed import #{invalid}"
+    puts "start import"
+    Movie.create!(data_list)
+    puts "success import"
+  rescue ActiveModel::UnknownAttributeError => invalid
+    puts "failed import #{invalid}"
+  end
+
+  # rake import_csv:text_data
+  def self.text_data(path = 'db/data/text.csv')
+    Text.delete_all
+    data_list = []
+    CSV.foreach(path, headers: true) do |row|
+      data_list << {
+          title: row["Title"],
+          contents: row["Contents"],
+          genre: row["Genre"]
+      }
     end
+    puts "start import"
+    Text.create!(data_list)
+    puts "success import"
+  rescue ActiveModel::UnknownAttributeError => invalid
+    puts "failed import #{invalid}"
   end
 
 end
