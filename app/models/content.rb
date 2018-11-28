1f3ffa64c807f50aa0ebda713d8ca254fa5bf90e
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-11-28 19:48:09 +0900

    content

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,8 +1,8 @@
 class Content
   def self.build_objects
     movies = Movie.order("id ASC")
-    texts = Text.all.order("id ASC")
-    lines = Line.all.order("id ASC")
+    texts = Text.order("id ASC")
+    lines = Line.order("id ASC")
     {movie: movies, text: texts, line: lines}
   end
 end
