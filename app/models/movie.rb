34f2fde461e45e8bd3e80c46057a9566e5a9b953
Yoshito Kamizato <yuyueureka157@gmail.com>
2019-01-22 09:28:09 +0900

    modify genre

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,26 +1,26 @@
 class Movie < ApplicationRecord
 
   PER_PAGE = 10
-  PROGRAMMING = ["Basic", "Git", "Ruby", "Ruby on Rails"]
+  PROGRAMMING = ["Basic", "git", "Ruby", "Ruby on Rails"]
 
   def self.count_level(page)
     page ? ((page.to_i * PER_PAGE) - (PER_PAGE - 1)) : 1
   end
 
   def self.disp_programming(page)
-    Movie.where(genre: PROGRAMMING).page(page).per(PER_PAGE)
+    Movie.where(genre: PROGRAMMING).order('id ASC').page(page).per(PER_PAGE)
   end
 
   def self.disp_money(page)
-    Movie.where(genre: "Money").page(page).per(PER_PAGE)
+    Movie.where(genre: "Money").order('id ASC').page(page).per(PER_PAGE)
   end
 
   def self.disp_design(page)
-    Movie.where(genre: "Design").page(page).per(PER_PAGE)
+    Movie.where(genre: "Design").order('id ASC').page(page).per(PER_PAGE)
   end
 
   def self.disp_salon(page)
-    Movie.where(genre: "Salon").order("id DESC").page(page).per(PER_PAGE)
+    Movie.where(genre: "Salon").order('id ASC').order("id DESC").page(page).per(PER_PAGE)
   end
 
 end
