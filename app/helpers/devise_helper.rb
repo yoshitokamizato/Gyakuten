3c6b2d158b74ec49879b57fac05a28977f223ec3
Yoshito Kamizato <yuyueureka157@gmail.com>
2019-06-02 15:59:49 +0900

    chenged error message of devise from en to ja

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,24 +1,20 @@
 module DeviseHelper
   def devise_error_messages!
-    return "" if resource.errors.empty?
+    return '' if flash.empty?
 
-    html = ""
-    resource.errors.full_messages.each do |errmsg|
-      html += <<-EOF
-      <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert">
-          <span aria-hidden="true">&times;</span>
-          <span class="sr-only">close</span>
-        </button>
-        #{errmsg}
-      </div>
-      EOF
+    html = ''
+
+    flash.each do |name, msg|
+      html += <<-HTML
+        <div class="alert alert-error alert-danger"> <button type="button"
+        class="close" data-dismiss="alert">x</button>
+          #{msg}
+        </div>
+      HTML
     end
-    html.html_safe
-  end
 
-  def devise_error_messages?
-    resource.errors.empty? ? false : true
+
+    html.html_safe
   end
 
   def resource_name
