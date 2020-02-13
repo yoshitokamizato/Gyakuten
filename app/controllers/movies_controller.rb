8fff5122b3e74c93da76672e8d7b5ba3bf5fe705
keitanaito <atiek.nn.xx@gmail.com>
2020-02-13 11:40:17 +0900

    movies.controller, 変数の統一
    各viewに渡す動画情報の入った変数を@moviesに統一する

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,24 +6,24 @@ class MoviesController < ApplicationController
     case params[:content_name]
     when "lives"
       @level = Movie.count_level(@page)
-      @salons = Movie.disp_lives(@page)
+      @movies = Movie.disp_lives(@page)
       render template: "lives/index"
 
     when "phps"
-      @php_movies = Movie.disp_phps
+      @movies = Movie.disp_phps
       render template: "phps/index"
 
     when "talks"
       @level = Movie.count_level(@page)
-      @talks = Movie.disp_talks(@page)
+      @movies = Movie.disp_talks(@page)
       render template: "talks/index"
 
     when "videoedittings"
-      @viedeoedittings = Movie.disp_modvieedittings
+      @movies = Movie.disp_modvieedittings
       render template: "videoedittings/index"
 
     when "writings"
-      @writing_movies = Movie.disp_writings
+      @movies = Movie.disp_writings
       render template: "writings/index"
 
     # params[:content_name]がない場合は動画教材ページを表示する
