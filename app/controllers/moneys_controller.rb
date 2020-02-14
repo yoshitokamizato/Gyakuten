157782d77f4b23c446cec3d12159d7c06e954872
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2020-02-14 19:37:43 +0900

    すぐに対応可能な lint を修正

--- app/controllers/moneys_controller.rb
+++ app/controllers/moneys_controller.rb
@@ -1,19 +1,14 @@
 class MoneysController < ApplicationController
   before_action :move_to_index, only: :index
-  before_action :get_page, only: :index
 
   def index
-    @level = Movie.count_level(@page)
-    @moneys = Movie.disp_money(@page)
+    @level = Movie.count_level(params[:page])
+    @moneys = Movie.disp_money(params[:page])
   end
 
   private
 
     def move_to_index
       redirect_to action: :index, flash: { error: "ログインしてください" } unless user_signed_in?
-    end
-
-    def get_page
-      @page = params[:page]
     end
 end
