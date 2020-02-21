2197c934277be8b3418328ea5d8dcd3551d2f845
tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2020-02-21 10:45:55 +0900

    movieモデルへコントローラー内のモデルロジックを移動（categorized_movies）
    （categorizeメソッド）
    ・コントローラーのparams（content_nameとpage）を引数に扱うメソッドへ
    ・movieモデルのgenreに応じて昇順、降順を選択するメソッドへ
    （initializer）
    ・constants.rbを作成し、共通で使用する定数をまとめて管理

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,42 +12,15 @@
 #
 
 class Movie < ApplicationRecord
-  PER_PAGE = 10
-  PROGRAMMING = ["Basic", "git", "Ruby", "Ruby on Rails", "AWS"].freeze
 
-  def self.count_level(page)
-    page ? ((page.to_i * PER_PAGE) - (PER_PAGE - 1)) : 1
-  end
-
-  def self.disp_programming(page)
-    Movie.where(genre: PROGRAMMING).order("id ASC").page(page).per(PER_PAGE)
-  end
-
-  def self.disp_money(page)
-    Movie.where(genre: "Money").order("id ASC").page(page).per(PER_PAGE)
-  end
-
-  def self.disp_salon(page)
-    Movie.where(genre: "Salon").order("id DESC").page(page).per(PER_PAGE)
-  end
-
-  def self.disp_talks(page)
-    Movie.where(genre: "Talk").order("id DESC").page(page).per(PER_PAGE)
-  end
-
-  def self.disp_modvieedittings(page)
-    Movie.where(genre: "Movie").order("id ASC").page(page).per(PER_PAGE)
-  end
-
-  def self.disp_writings(page)
-    Movie.where(genre: "Writing").order("id ASC").page(page).per(PER_PAGE)
-  end
-
-  def self.disp_phps(page)
-    Movie.where(genre: "Php").order("id ASC").page(page).per(PER_PAGE)
-  end
-
-  def self.disp_lives(page)
-    Movie.where(genre: "Live").order("id DESC").page(page).per(PER_PAGE)
+  def self.categorize(content_name, page)
+    case content_name
+    when "Money", "Movie", "Writing", "Php" then
+      self.where(genre: content_name).order("id ASC").page(page).per(PER_PAGE)
+    when "Salon", "Talk", "Live" then
+      self.where(genre: content_name).order("id DESC").page(page).per(PER_PAGE)
+    else
+      self.where(genre: PROGRAMMING).order("id ASC").page(page).per(PER_PAGE)
+    end
   end
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,4 @@
+# 1ページの動画表示件数を指定
+PER_PAGE = 10
+# ナビゲーションバーで特に指定しない動画のジャンルをまとめる
+PROGRAMMING = ["Basic", "git", "Ruby", "Ruby on Rails"].freeze
