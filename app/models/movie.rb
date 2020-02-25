2eaf0f9bb81c799d6048a0bf8522e70b80e12dd8
tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2020-02-25 19:22:47 +0900

    定数宣言箇所のインデント修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,10 +12,10 @@
 #
 
 class Movie < ApplicationRecord
-# 1ページの動画表示件数を指定
-PER_PAGE = 10
-# ナビゲーションバーで特に指定しない動画のジャンルをまとめて格納
-PROGRAMMING = ["Basic", "git", "Ruby", "Ruby on Rails"].freeze
+  # 1ページの動画表示件数を指定
+  PER_PAGE = 10
+  # ナビゲーションバーで特に指定しない動画のジャンルをまとめて格納
+  PROGRAMMING = ["Basic", "git", "Ruby", "Ruby on Rails"].freeze
 
   def self.categorized_by(genre: genre, page:)
     case genre
