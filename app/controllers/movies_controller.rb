d503e95cb29dd3d00bfddc22c63b826149629d6e
tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2020-02-28 14:14:59 +0900

    movieモデルcatogorized_byメソッドの引数をメソッド名に関連するよう修正
    ・categorized_by(genre, page:)に変更、メソッド名と引数が関連しやすいように修正
    ・モデルの引数変更に伴い、コントローラーの引数を変更

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,6 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @movies = Movie.categorized_by(genre: params[:genre], page: params[:page])
+    @movies = Movie.categorized_by(params[:genre], page: params[:page])
   end
 end
