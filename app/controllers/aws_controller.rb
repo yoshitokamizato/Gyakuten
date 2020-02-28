c198cd301de4b8f9a00ab4293a986619bf413e50
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2020-02-28 22:24:46 +0900

    fix lint

--- app/controllers/aws_controller.rb
+++ app/controllers/aws_controller.rb
@@ -2,7 +2,7 @@ class AwsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @aws = Aw.order('id ASC')
+    @aws = Aw.order("id ASC")
   end
 
   def show
@@ -11,8 +11,7 @@ class AwsController < ApplicationController
 
   private
 
-  def move_to_index
-    redirect_to action: :index, flash: {error: "ログインしてください"} unless user_signed_in?
-  end
-
+    def move_to_index
+      redirect_to action: :index, flash: { error: "ログインしてください" } unless user_signed_in?
+    end
 end
