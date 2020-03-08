c6e8127542f58fa520cf74b38c60786726f696ea
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2020-03-08 21:41:09 +0900

    move_to_index は不要になったので一括削除

--- app/controllers/marketings_controller.rb
+++ app/controllers/marketings_controller.rb
@@ -1,11 +1,9 @@
 class MarketingsController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index
 
   def index
     page = params[:page]
     @level = Marketing.count_level(page)
     @marketings = Marketing.search_all_movie(page)
   end
-
 end
