cb9f152645f958ce07d24e5f1acfbefb6c33e5f0
福岡和樹 <kazuki@mba.local>
2020-05-04 14:14:43 +0900

    moviesテーブルのpositionをid順に設定

--- db/migrate/20200502051652_add_column_position_to_movies.rb
+++ db/migrate/20200502051652_add_column_position_to_movies.rb
@@ -1,5 +1,11 @@
 class AddColumnPositionToMovies < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :movies, :position, :integer
+    Movie.order(id: :asc).each.with_index(1) do |movie, index|
+      movie.update(position: index)
+    end
+  end
+  def down
+    remove_column :movies, :position, :integer
   end
 end
