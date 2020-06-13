f8eb2cefdf53671e0cc8e06dfc3be9346b4f698f
T.Tsujii <ttakehisabt@gmail.com>
2020-06-14 08:13:06 +0900

    管理者画面の表示内容を修正

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,29 @@
 ActiveAdmin.register User do
-  permit_params :name, :email
+  permit_params :email, :slack_id, :flag
+
+  index do
+    selectable_column
+    id_column
+    column :flag
+    column :slack_id
+    column :email
+    column :current_sign_in_at
+    column :created_at
+    actions
+  end
+
+  filter :flag
+  filter :slack_id
+  filter :email
+  filter :current_sign_in_at
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :flag
+      f.input :slack_id
+      f.input :email
+    end
+    f.actions
+  end
 end
