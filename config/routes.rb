4050141c6d4d3f82154d8c809e22adc3a433b21e
T.Tsujii <ttakehisabt@gmail.com>
2020-06-15 13:32:07 +0900

    カスタマイズ用の devise コントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "movies#index"
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
