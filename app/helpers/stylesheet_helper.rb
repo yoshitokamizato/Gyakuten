8c2fbf0291743c7d1837c2b9fd1268f627d6541b
tanahara <s10.vermouth.4869@gmail.com>
2020-10-14 15:12:35 +0900

    viewportを導入し, headerとfooterの背景色を調整

---
+++ app/helpers/stylesheet_helper.rb
@@ -0,0 +1,15 @@
+module StylesheetHelper
+  def base_color
+    controller_name == "lines" ? "line-color" : "base-color"
+  end
+
+  def max_width
+    if devise_controller?
+      "mw-sm"
+    elsif controller_name.in?(%w[my_pages movies]) || (controller_name == "texts" && action_name == "index")
+      "mw-xl"
+    else
+      "mw-md"
+    end
+  end
+end
