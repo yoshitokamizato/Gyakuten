d0e59d9f411710b6cea9ea02bdd5e3787bc4e418
tanahara <s10.vermouth.4869@gmail.com>
2020-10-20 20:47:48 +0900

    rubocop -a を実行

--- lib/autoloads/custom_markdown_renderer.rb
+++ lib/autoloads/custom_markdown_renderer.rb
@@ -43,33 +43,33 @@ class CustomMarkdownRenderer < Redcarpet::Render::HTML
 
   private
 
-  def split_file_and_lang(options)
-    # options は４通りの形式に対応できるようにする
-    # 1. 無し
-    # 2. html
-    # 3. app/views/layouts/application.html.erb
-    # 4. html:app/views/layouts/application.html.erb
-    if options.blank?
-      # 1. options が無いケース
-      filename = nil
-      language = "md"
-    else
-      # 2 〜 4 のケース
-      array = options.split(":")
-      if array[0].include?(".")
-        # 3. app/views/layouts/application.html.erb のケース
-        filename = array[0]
-        language = array[0].split(".")[-1]
-      elsif array.length > 1
-        # 4. html:app/views/layouts/application.html.erb のケース
-        filename = array[1]
-        language = array[0]
-      else
-        # 2. html のケース
+    def split_file_and_lang(options)
+      # options は４通りの形式に対応できるようにする
+      # 1. 無し
+      # 2. html
+      # 3. app/views/layouts/application.html.erb
+      # 4. html:app/views/layouts/application.html.erb
+      if options.blank?
+        # 1. options が無いケース
         filename = nil
-        language = array[0]
+        language = "md"
+      else
+        # 2 〜 4 のケース
+        array = options.split(":")
+        if array[0].include?(".")
+          # 3. app/views/layouts/application.html.erb のケース
+          filename = array[0]
+          language = array[0].split(".")[-1]
+        elsif array.length > 1
+          # 4. html:app/views/layouts/application.html.erb のケース
+          filename = array[1]
+          language = array[0]
+        else
+          # 2. html のケース
+          filename = nil
+          language = array[0]
+        end
       end
+      [filename, language]
     end
-    [filename, language]
-  end
 end
