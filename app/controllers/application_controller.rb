0909c4bf898ca12806e8089621010c6766023ee7
T.Tsujii <ttakehisabt@gmail.com>
2021-01-25 23:24:32 +0900

    run bundle exec rubocop -A

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,15 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit :sign_up, keys: [:slack_id]
-  end
-
-  def sign_out_user
-    unless current_user.flag
-      flash.clear
-      flash.now[:alert] = "現在参加中のサロンのSlack IDで新規登録をお願いいたします。"
-      sign_out(current_user)
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit :sign_up, keys: [:slack_id]
     end
-  end
+
+    def sign_out_user
+      unless current_user.flag
+        flash.clear
+        flash.now[:alert] = "現在参加中のサロンのSlack IDで新規登録をお願いいたします。"
+        sign_out(current_user)
+      end
+    end
 end
