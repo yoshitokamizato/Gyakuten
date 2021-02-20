4be920547e598c656c4ab106a2b03fd241ba92e3
新田陸人 <>
2021-02-20 14:46:24 +0900

    トップページへのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root to: "texts#index"
+  root to: "static_pages#home"
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
@@ -8,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :static_pages, only: :home
   resource :my_pages, only: :show
   resources :texts, only: [:index, :show] do
     resource :read_texts, only: [:create, :destroy]
