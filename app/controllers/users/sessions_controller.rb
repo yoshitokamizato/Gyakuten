61326d2db98793902dbb9ce5c672ce540d4171a1
T.Tsujii <ttakehisabt@gmail.com>
2021-02-24 21:41:07 +0900

    コメントを修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,7 +12,7 @@ class Users::SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate!(auth_options)
     # Deviseのログイン維持期間はデフォルトの2週間で設定中
-    # やんばるCODEの方は，前回のチェックから1週間以上経過している場合は，再度Slackに問い合わせ，
+    # やんばるCODEの方は，前回のチェックから1週間以上経過している場合は，再度Slackに問い合わせる
     # 削除済みアカウントになっている場合は承認を取り消す
     # ただし，本番環境以外では問い合わせを行わないとする
     if Rails.env.production? && resource.flag && resource.slack_name == "yanbaru_code" && Time.current > resource.approval_at + 7.days
