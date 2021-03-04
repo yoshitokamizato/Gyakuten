1efa230ad406224a0769f9a5d3022b82fbe9df11
T.Tsujii <ttakehisabt@gmail.com>
2021-03-04 09:16:33 +0900

    不要なマイグレーションファイルを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,23 @@
 require "import_csv"
+
+return unless Rails.env.development?
+
 EMAIL = "test@example.com".freeze
 ADMIN_EMAIL = "admin@example.com".freeze
 PASSWORD = "password".freeze
 SLACK_ID = "00000000".freeze
 SLACK_NAME = "other"
 
-if Rails.env.development?
-  User.find_or_create_by!(email: EMAIL) do |user|
-    user.password = PASSWORD
-    user.slack_id = SLACK_ID
-    user.slack_name = SLACK_NAME
-    user.flag = true
-    puts "ユーザーの初期データインポートに成功しました。"
-  end
-  AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |user|
-    user.password = PASSWORD
-    puts "管理者ユーザーの初期データインポートに成功しました。"
-  end
-  ImportCsv.execute(model: Genre)
+User.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  user.slack_id = SLACK_ID
+  user.slack_name = SLACK_NAME
+  user.flag = true
+  puts "ユーザーの初期データインポートに成功しました。"
 end
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |user|
+  user.password = PASSWORD
+  puts "管理者ユーザーの初期データインポートに成功しました。"
+end
+ImportCsv.execute(model: Genre)
+
