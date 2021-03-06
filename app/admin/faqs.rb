17f2679413a830c08a14cc0730e6120f5e285a25
T.Tsujii <ttakehisabt@gmail.com>
2021-03-07 07:31:25 +0900

    faqsテーブルの管理者画面表示を修正

--- app/admin/faqs.rb
+++ app/admin/faqs.rb
@@ -1,3 +1,32 @@
 ActiveAdmin.register Faq do
   permit_params :position, :question, :answer
+  config.sort_order = "position_asc"
+
+  index do
+    selectable_column
+    column :position
+    column :question
+    column :answer
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :position
+      row :question
+      row :answer
+    end
+  end
+
+  form do |f|
+    f.object.position ||= Faq.maximum(:position) + 1
+    f.semantic_errors
+    f.inputs do
+      input :position
+      input :question
+      input :answer
+    end
+    f.actions
+  end
 end
